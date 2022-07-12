7b606a02581ebe5bd92c792accac3e2e3fd4140f
sh <1659411011@qq.com>
2022-07-12 10:13:06 +0800

    feat: init preset

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
     <el-button type="primary">primary</el-button>
   </div>
 </template>
---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="hello">111</div>
+</template>
+
+<script>
+export default {};
+</script>
